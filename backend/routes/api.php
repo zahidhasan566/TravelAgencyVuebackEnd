0168a5f5416bacd0d82a646462c3f3d12c9725e6
zahidDaraz <zahidh.daraz@gmail.com>
2022-10-13 17:44:24 +0600

    s6

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -30,6 +30,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/dashboard', [\App\Http\Controllers\DashboardController::class, 'dashboard']);
     Route::post('/admin/create-blog-post', [\App\Http\Controllers\AdminBlogController::class, 'index']);
+    Route::post('/admin/view-blog-post', [\App\Http\Controllers\AdminBlogController::class, 'view_post']);
 });
 
 Route::post('/test', [\App\Http\Controllers\TestController::class, 'receive_msg']);
@@ -40,7 +41,6 @@ Route::post('/customer/update/confirm', [\App\Http\Controllers\CustomerControlle
 Route::post('/customer/delete', [\App\Http\Controllers\CustomerController::class, 'delete']);
 Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login']);
 Route::post('/logout', [\App\Http\Controllers\LoginController::class, 'logout']);
-Route::post('/admin/view-blog-post', [\App\Http\Controllers\AdminBlogController::class, 'view_post']);
 Route::post('/admin/delete-blog-post', [\App\Http\Controllers\AdminBlogController::class, 'delete_post']);
 Route::post('/blog/update', [\App\Http\Controllers\AdminBlogController::class, 'update']);
 Route::post('/blog/update/confirm', [\App\Http\Controllers\AdminBlogController::class, 'blog_update_confirm']);
