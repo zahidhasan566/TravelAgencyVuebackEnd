b409fd71b7ab4771baacb036b0a63d6642c4774c
zahidDaraz <zahidh.daraz@gmail.com>
2022-10-13 17:11:29 +0600

    s3

--- backend/public/index.php
+++ backend/public/index.php
@@ -15,13 +15,6 @@ define('LARAVEL_START', microtime(true));
 | instead of starting the framework, which could cause an exception.
 |
 */
-header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers:*");
-
-if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {//send back preflight request response
-    return "";
-}
 if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
     require __DIR__.'/../storage/framework/maintenance.php';
 }
