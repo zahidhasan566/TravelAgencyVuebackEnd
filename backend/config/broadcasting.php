2369d00f6e6a2c0e81153989ec8adf2c8a086a53
zahidDaraz <zahidh.daraz@gmail.com>
2022-10-16 16:17:11 +0600

    16-136

--- backend/config/broadcasting.php
+++ backend/config/broadcasting.php
@@ -40,7 +40,10 @@ return [
                 'useTLS' => true,
                 'host'=>'127.0.0.1',
                 'port'=> 6001,
-                'scheme'=>'http'
+                'scheme'=>'http',
+                'options'=>[
+                    'encrypted' =>'false'
+                ],
             ],
         ],
 
