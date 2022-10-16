5edf355276f4a3aba9067efdd6a4467774b22095
zahidDaraz <zahidh.daraz@gmail.com>
2022-10-16 10:19:34 +0600

    16-4

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -19,6 +19,8 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*') // maybe put this into the .env file so you can change the URL in production.
             ->header('Access-Control-Allow-Methods', '*') // or specify `'GET, POST, PUT, DELETE'` etc as the second parameter if you want to restrict the methods that are allowed.
-            ->header('Access-Control-Allow-Headers', 'POST, PUT'); // or add your headers.
+            ->header('Access-Control-Allow-Headers', 'POST, PUT')
+            ->header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token')
+            ->header("Access-Control-Allow-Credentials", "true");;// or add your headers.
     }
 }
